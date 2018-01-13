c7043e9d9344c876f0ae41abb57a9df4635bcac0
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-01-13 10:12:55 -0600

    C# program first initialization

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace frogFitness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Prompt user for minutes exercised
+           System.Console.Write("Enter how many minutes you exercised: ");
+           //Add minutes exercised to total
+           //Display total munutes exercises to screen
+           //Repeat until user quits
+
+        }
+    }
+}
