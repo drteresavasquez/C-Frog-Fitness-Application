7b2db1d3ca8cddd1295d7e3e42ebec9cc015b2e5
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-01-13 10:37:35 -0600

    updated to accept user input

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ namespace frogFitness
         {
            //Prompt user for minutes exercised
            System.Console.Write("Enter how many minutes you exercised: ");
+
+           string entry = System.Console.ReadLine();
+
            //Add minutes exercised to total
            //Display total munutes exercises to screen
            //Repeat until user quits
