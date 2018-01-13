65cbef3c7db58b89b1edbe62c77823c19e6eee75
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-01-13 11:31:06 -0600

    added a parse to convert user input into an integer

--- Program.cs
+++ Program.cs
@@ -6,14 +6,19 @@ namespace frogFitness
     {
         static void Main(string[] args)
         {
+            int runningTotal = 0;
            //Prompt user for minutes exercised
            Console.Write("Enter how many minutes you exercised: ");
 
            string entry = Console.ReadLine();
 
+           //converts string input to a number
+           int minutes = int.Parse(entry);
+           runningTotal = runningTotal + minutes;
+
            //Add minutes exercised to total
            //Display total minutes exercises to screen
-           Console.WriteLine("You've entered " + entry + " minutes!");
+           Console.WriteLine("You've entered " + runningTotal + " minutes!");
 
 
            //Repeat until user quits
