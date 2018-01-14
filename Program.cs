24e17b0a55e0d4104f912d8ff1759dc822c98c61
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-01-14 12:59:24 -0600

    updated app to take minutes or quit

--- Program.cs
+++ Program.cs
@@ -7,22 +7,31 @@ namespace frogFitness
         static void Main(string[] args)
         {
             int runningTotal = 0;
-           //Prompt user for minutes exercised
-           Console.Write("Enter how many minutes you exercised: ");
+            bool keepGoing = true;
 
-           string entry = Console.ReadLine();
+          while(keepGoing)
+          {
+               //Prompt user for minutes exercised
+                Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
 
-           //converts string input to a number
-           int minutes = int.Parse(entry);
-           runningTotal = runningTotal + minutes;
+                string entry = Console.ReadLine();
 
-           //Add minutes exercised to total
-           //Display total minutes exercises to screen
-           Console.WriteLine("You've entered " + runningTotal + " minutes!");
+                if (entry == "quit")
+                {
+                    keepGoing = false;
+                }
+                else{
+                    //converts string input to a number
+                    int minutes = int.Parse(entry);
+                    runningTotal = runningTotal + minutes;
 
-
-           //Repeat until user quits
-
+                    //Add minutes exercised to total
+                    //Display total minutes exercises to screen
+                    Console.WriteLine("You've entered " + runningTotal + " minutes!");
+                }
+                //Repeat until user quits
+            };
+            Console.WriteLine("Goodbye!");
         }
     }
 }
